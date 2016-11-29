95a540285007eb133f3a4d81fe880033d8f4abde
Nonun <jcox3@berklee.edu>
2016-11-29 14:18:51 -0500

    Soundmanager
    SoundManager Jessie’s Function

---
+++ Assets/SoundManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundManager : MonoBehaviour {
+
+
+	public static SoundManager instance;
+
+	public static void JessieFunction (GameObject objectForAudio) {
+		//an array to hold the sounds
+AudioClip[] allSounds;
+
+		//load sounds into the array
+		allSounds = Resources.LoadAll<AudioClip>("Sounds");
+	}
+}
