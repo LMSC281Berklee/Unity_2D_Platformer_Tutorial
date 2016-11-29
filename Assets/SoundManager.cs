19130d2d62b3f47fc45041ccde308d6be6af2c76
antonioespinosah <aespinosa@berklee.edu>
2016-11-29 14:50:21 -0500

    Final

--- Assets/SoundManager.cs
+++ Assets/SoundManager.cs
@@ -2,15 +2,40 @@
 using System.Collections;
 
 public class SoundManager : MonoBehaviour {
-
-
+	public GameObject spawner;
 	public static SoundManager instance;
+	public static AudioClip[] allSounds;
+	public static AudioSource maintheme;
+	public static bool mainthemebool = true;
+	public static void JessieFunction (GameObject objectForAudio) 
 
-	public static void JessieFunction (GameObject objectForAudio) {
-		//an array to hold the sounds
-AudioClip[] allSounds;
+	{
+		//an array to hold the sounds (Jessie)
 
-		//load sounds into the array
-		allSounds = Resources.LoadAll<AudioClip>("Sounds");
+
+		//load sounds into the array (Jessie)
+		allSounds = Resources.LoadAll<AudioClip>("Enemy");
 	}
+	void start(){ 
+		spawner = GameObject.Find ("spawner");
+		mainthemebool = true;
+	}
+	//Function to load random sound on collision (Antonio)
+	void PlayFunction (){
+		if (mainthemebool = true) {
+			maintheme.Play ();
+		}
+	}
+	void OnCollisionEnter (Collision col)
+	{
+		if(col.gameObject)
+		{
+			AudioClip mysound = allSounds [Random.Range (0, allSounds.Length)];
+				}
+		//Function to loop main music (Daniya)
+		if (mainthemebool = false) {
+			maintheme.Play ();
+		}
+		
+}
 }
